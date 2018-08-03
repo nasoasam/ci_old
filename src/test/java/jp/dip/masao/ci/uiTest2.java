d9065f6506fbd0814a03e05325c83a172485ae84
Masao Yamauchi <nasoasam@hotmail.com>
2018-08-03 19:57:27 +0900

    uiTest5

---
+++ src/test/java/jp/dip/masao/ci/uiTest2.java
@@ -0,0 +1,20 @@
+package jp.dip.masao.ci;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import org.junit.Test;
+
+public class uiTest2 {
+
+	@Test
+	public void contextLoads() {
+//		Configuration.browser = WebDriverRunner.CHROME;
+//		Configuration.baseUrl = "https://www.yahoo.co.jp";
+//		Configuration.baseUrl = "http://10.33.0.100:60000";
+		open("/");
+		open("/");
+		open("/");
+		open("/");
+		open("/");
+	}
+}
