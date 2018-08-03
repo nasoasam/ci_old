ac3fd4992fdfcf48a1818fd639ea44d3c4273c88
Masao Yamauchi <nasoasam@hotmail.com>
2018-08-03 11:06:59 +0900

    first commit

---
+++ src/main/java/jp/dip/masao/ci/HelloController.java
@@ -0,0 +1,11 @@
+package jp.dip.masao.ci;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hollow World!";
+    }
+}
